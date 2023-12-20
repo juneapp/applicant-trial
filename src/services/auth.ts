31f8426cd3928f41d010cdd6d3140e390d56b572
Leon Gieser <leon.gieser@gmail.com>
2023-12-20 19:03:34 +0100

    :passport_control: auth service

---
+++ src/services/auth.ts
@@ -0,0 +1,29 @@
+import { API_BASE_URL } from "../constants";
+
+export async function loginWithUserNameAndPassword(
+    username: string,
+    password: string
+) {
+    const response = await fetch(`${API_BASE_URL}/auth/login_check`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ username, password }),
+    });
+
+    if (!response.ok) {
+        throw new Error("Login failed");
+    }
+
+    const data = await response.json();
+    const token = data.token;
+
+    // document.cookie = `token=${token}; max-age=${60}`;
+    document.cookie = `token=${token}; max-age=${60 * 60 * 24 * 7}`;
+}
+
+export function logout() {
+    document.cookie = "token=; max-age=0";
+    window.location.reload();
+}
