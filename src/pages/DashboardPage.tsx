8212cd9cdb39bb262aa1977fcc39f3779e8006a9
Leon Gieser <leon.gieser@gmail.com>
2023-12-21 07:20:15 +0100

    :lipstick: Dashboard Page

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -1,18 +1,27 @@
 import { useState, useEffect } from "react";
 import { getProjects } from "../services/";
+import { ProjectCard } from "../components";
+import "./DashboardPage.css";
+import { Project } from "../types";
+import { Icons } from "../icons";
 
 export function DashboardPage() {
-    const [projects, setProjects] = useState([]);
+    const [projects, setProjects] = useState<Project[] | []>([]);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
 
     useEffect(() => {
         let isMounted = true;
 
         const fetchProjects = async () => {
             try {
-                const projects = await getProjects();
-                console.log(projects);
+                //? Show loading skeleton
+                await new Promise((resolve) => setTimeout(resolve, 10000));
+
+                const projects = (await getProjects()) as Project[];
+
                 if (isMounted) {
                     setProjects(projects);
+                    setIsLoading(false);
                 }
             } catch (error) {
                 console.error(error);
@@ -26,15 +35,35 @@ export function DashboardPage() {
         };
     }, []);
 
-    //TODO style grid and page
     return (
-        <div>
-            <h3>
-                <pre>{JSON.stringify(projects, null, 2)}</pre>
-                {/* {projects.map((p: Project, idx) => (
-                    <div key={idx}>{p.title}</div>
-                ))} */}
-            </h3>
+        <div className="dashboard-page">
+            <h1 className="dashboard-page__title">
+                Pro<span className="text-color-main">j</span>ects Overview
+            </h1>
+            <section className="project-grid">
+                {isLoading ? (
+                    <>
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                        <SkeletonCard />
+                    </>
+                ) : (
+                    <>
+                        <article className="dashboard-page__new-project-card">
+                            <Icons.PlusSign className="dashboard-page__new-project-card--icon" />
+                        </article>
+                        {projects.map((project: Project) => (
+                            <ProjectCard key={project.id} project={project} />
+                        ))}
+                    </>
+                )}
+            </section>
         </div>
     );
 }
+
+const SkeletonCard = () => {
+    return <article className="dashboard-page__card-skeleton"></article>;
+};
